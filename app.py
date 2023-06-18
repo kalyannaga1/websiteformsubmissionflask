25915a229e8bf33db880715bc6e37e48d45a6aeb
JaideepBgit <bjaideep021@gmail.com>
2023-06-17 21:40:22 -0500

    change the app to take lname and form to email

--- app.py
+++ app.py
@@ -11,18 +11,20 @@ class User(db.Model):
     __tablename__ = "Users"
 
     fName = db.Column(db.String, primary_key=True)
+    lName = db.Column(db.String)  # Added this line
     email = db.Column(db.String)
 
 @app.route('/', methods=['GET', 'POST'])
 def form_submit():
     if request.method == 'POST':
         fName = request.form.get('fname')
+        lName = request.form.get('lname')  # Added this line
         email = request.form.get('email')
 
-        user = User(fName=fName, email=email)
+        user = User(fName=fName, lName=lName, email=email)  # Added lName in this line
         db.session.add(user)
         db.session.commit()
-        
+
         return 'Form submitted successfully!'
     
     return render_template('submitform.html')
