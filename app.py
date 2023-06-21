fce85a5b561b54995cc28d51e4edb98adfdf815b
JaideepBgit <bjaideep021@gmail.com>
2023-06-21 15:44:03 -0500

    add files

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import urllib
+
+params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=formsubmissionserver.database.windows.net;DATABASE=Formsubmission;UID=kalyan;PWD=qwertynaga@1")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "Users"
+
+    fName = db.Column(db.String, primary_key=True)
+    lName = db.Column(db.String)  # Added this line
+    email = db.Column(db.String)
+
+@app.route('/', methods=['GET', 'POST'])
+def form_submit():
+    if request.method == 'POST':
+        fName = request.form.get('fname')
+        lName = request.form.get('lname')  # Added this line
+        email = request.form.get('email')
+
+        user = User(fName=fName, lName=lName, email=email)  # Added lName in this line
+        db.session.add(user)
+        db.session.commit()
+
+        return 'Form submitted successfully!'
+    
+    return render_template('submitform.html')
+
+if __name__ == '__main__':
+    app.run(port=8080)
